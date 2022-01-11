44e9affde30d0a095ae5d1574a8fa8321001053b
Jason <jasonkelly6@gmail.com>
2022-01-10 20:54:23 -0500

    set env variables and hide secrets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     TasksModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'postgres',
-      password: 'postgres',
+      host: process.env.HOST,
+      port: Number(process.env.PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
       database: 'task-management',
       autoLoadEntities: true,
       synchronize: true,
